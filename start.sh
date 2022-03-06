fc4f60b6bb93112126da9197919fdb8901507842
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2022-03-06 17:23:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/akhilbaiju/VideoStreamerBot -b $BRANCH /VideoStreamerBot
 fi
-cd /VCPlayerBot
+cd /VideoStreamerBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
